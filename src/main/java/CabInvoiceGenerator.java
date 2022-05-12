ae31ea03214440363956c626d5279063f49b23ba
manjugoutham <gouthamy219@gmail.com>
2022-05-12 16:20:30 +0530

    Updated code latest changes

--- src/main/java/CabInvoiceGenerator.java
+++ src/main/java/CabInvoiceGenerator.java
@@ -3,8 +3,6 @@ public class CabInvoiceGenerator {
     public static final double COSTPerKM = 10.0;
     public static final double COSTPerMIN = 1.0;
     public static final double MIN_FARE = 5;
-
-    // UC-1 method to check the total fare for the journey
     public double calculateFare(double distance, int time) {
         double totalFare = (distance * COSTPerKM) + (time * COSTPerMIN);
         if (totalFare < 5) {
--- src/test/java/CabInvoiceTest.java
+++ src/test/java/CabInvoiceTest.java
@@ -2,13 +2,13 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class CabInvoiceTest {
-    CabInvoiceGenerator invoiceGenerator = new CabInvoiceGenerator();
+    CabInvoiceGenerator cabinvoiceGenerator = new CabInvoiceGenerator();
     // UC-1 test method to check the fare returned through the method
     @Test
     public void givenDistanceAndTime_ShouldReturnFare() {
         double distance = 2.0;
         int time = 5;
-        double result = invoiceGenerator.calculateFare(distance, time);
+        double result = cabinvoiceGeneratorgit status.calculateFare(distance, time);
         Assert.assertEquals(25, result,0);
     }
 }
